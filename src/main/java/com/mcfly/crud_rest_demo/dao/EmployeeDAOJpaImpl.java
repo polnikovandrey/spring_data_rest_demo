727cd9fe9697f2f0ccf7008bfe0a50d614418cbc
polnikov.andrey <polnikov.andrey1981@gmail.com>
2023-05-16 23:47:20 +0300

    + EmployeeDao & EmployeeService CRUD methods

--- src/main/java/com/mcfly/crud_rest_demo/dao/EmployeeDAOJpaImpl.java
+++ src/main/java/com/mcfly/crud_rest_demo/dao/EmployeeDAOJpaImpl.java
@@ -21,4 +21,20 @@ public class EmployeeDAOJpaImpl implements EmployeeDAO {
     public List<Employee> findAll() {
         return entityManager.createQuery("from Employee", Employee.class).getResultList();
     }
+
+    @Override
+    public Employee findById(int id) {
+        return entityManager.find(Employee.class, id);
+    }
+
+    @Override
+    public Employee save(Employee employee) {
+        return entityManager.merge(employee);
+    }
+
+    @Override
+    public void deleteById(int id) {
+        final Employee employee = entityManager.find(Employee.class, id);
+        entityManager.remove(employee);
+    }
 }
