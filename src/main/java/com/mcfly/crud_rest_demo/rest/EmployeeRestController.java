370b55d28845babcccb0afdb6d7c7c7e83cb5ad4
polnikov.andrey <polnikov.andrey1981@gmail.com>
2023-05-17 00:10:46 +0300

    + EmployeeRestController CRUD methods

--- src/main/java/com/mcfly/crud_rest_demo/rest/EmployeeRestController.java
+++ src/main/java/com/mcfly/crud_rest_demo/rest/EmployeeRestController.java
@@ -3,9 +3,7 @@ package com.mcfly.crud_rest_demo.rest;
 import com.mcfly.crud_rest_demo.entity.Employee;
 import com.mcfly.crud_rest_demo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +21,35 @@ public class EmployeeRestController {
     @GetMapping("/employees")
     public List<Employee> findAllEmployees() {
         return employeeService.findAll();
+    }
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee findById(@PathVariable int employeeId) {
+        final Employee employee = employeeService.findById(employeeId);
+        if (employee == null) {
+            throw new RuntimeException("Employee id not found: " + employeeId);
+        }
+        return employee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee employee) {
+        employee.setId(0);      // Ensure there is no id to guarantee that a new employee will be created instead of update existing.
+        return employeeService.save(employee);
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+        return employeeService.save(employee);
+    }
+
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId) {
+        final Employee employee = employeeService.findById(employeeId);
+        if (employee == null) {
+            throw new RuntimeException("Employee id not found: " + employeeId);
+        }
+        employeeService.deleteById(employeeId);
+        return "Deleted employee id: " + employeeId;
     }
 }
