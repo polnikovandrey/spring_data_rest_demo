54cb58f14ddf9ed588f12616b6e0437fc00b837a
polnikov.andrey <polnikov.andrey1981@gmail.com>
2023-05-16 23:14:54 +0300

    + EmployeeRestController

---
+++ src/main/java/com/mcfly/crud_rest_demo/rest/EmployeeRestController.java
@@ -0,0 +1,27 @@
+package com.mcfly.crud_rest_demo.rest;
+
+import com.mcfly.crud_rest_demo.dao.EmployeeDAO;
+import com.mcfly.crud_rest_demo.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+
+    private final EmployeeDAO employeeDAO;
+
+    @Autowired
+    public EmployeeRestController(EmployeeDAO employeeDAO) {
+        this.employeeDAO = employeeDAO;
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> findAllEmployees() {
+        return employeeDAO.findAll();
+    }
+}
