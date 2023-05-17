908a4c027bd62cd17e11573936dde92093f80ce7
polnikov.andrey <polnikov.andrey1981@gmail.com>
2023-05-17 09:54:41 +0300

    + customized rest api endpoint name + custom rest api default page size

--- src/main/java/com/mcfly/crud_rest_demo/dao/EmployeeRepository.java
+++ src/main/java/com/mcfly/crud_rest_demo/dao/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.mcfly.crud_rest_demo.dao;
 
 import com.mcfly.crud_rest_demo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="members")     // Custom api endpoint name instead of the default generated "employees".
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
